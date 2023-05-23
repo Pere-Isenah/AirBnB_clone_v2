eaa9cb5bd4d5f391cfa935192d9114696088fe60
Pere-Isenah <pereisenah@gmail.com>
2023-05-24 00:35:19 +0100

    task1-6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -58,7 +58,7 @@ def number_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-     """
+    """
     Determine if a number is odd or even.
 
     Args:
